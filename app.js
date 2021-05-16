e620c2ac7147f498e0dffbbd164aa33bbdcefa33
brokylabs <c.meong@gmail.com>
2021-05-16 20:12:03 +0700

    Endpoint payment dan confirmation

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const pug   = require('pug')
 const _ = require('lodash')
 const path = require('path')
 const {Donor} = require('./model/donor')
+const { response } = require('express')
 const {initializePayment, verifyPayment} = require('./config/paystack')(request);
 
 const port  = process.env.PORT || 3000;
@@ -14,3 +15,73 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extends : false}));
 app.use(express.static(path.join(__dirname, 'public/')));
 app.set('view engine', pug);
+
+app.get('/', (req, res) => {
+    res.render('index.pug')
+})
+
+app.post('/paystack/pay', (req, res) => {
+    const form = _.pick(req.body, ['amount', 'email', 'full_name']);
+    form.metadata = {
+        full_name : form.full_name
+    }
+    form.amount * = 100;
+
+    initializePayment(form, (error, body) => {
+        if(error){
+            console.log(error);
+            return res.redirect('/error')
+            return;
+        }
+        response = JSON.parse(body);
+        res.redirect(response.data.authorization_url)
+    });
+});
+
+app.get('/paystack/callback', (req, res) => {
+    const ref = req.query.reference;
+    verifyPayment(ref, (error, body) => {
+        if(error){
+            console.log(error);
+            return res.redirect('/error');
+        }
+        response = JSON.parse(body)
+
+        const data = _.at(response.data, ['reference', 'amount', 'customer.email', 'metadata.full_name']);
+
+        [reference, amount, email, full_name] = data;
+
+        newDonor = {reference, amount, email, full_name}
+
+        const donor = new Donor(newDonor)
+
+        donor.save().then((donor) => {
+            if(!donor){
+                return res.redirect('/error')
+            }
+            res.redirect('/receipt/'+donor._id);
+        }).catch((e) => {
+            res.redirect('/error')
+        })
+    })
+})
+
+app.get('/receipt/:id', (req, res) => {
+    const id = req.param.id;
+    Donor.findById(id).then((donor) => {
+        if(!donor){
+            res.redirect('/error')
+        }
+        res.redirect('success.pug', (donor))
+    }).catch((e) => {
+        res.redirect('/error')
+    })
+})
+
+app.get('/error', (req, res) => {
+    res.render('error.pug')
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+})
